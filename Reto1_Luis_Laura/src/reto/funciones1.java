4a245cd80d7578e3cbf5957425562fd6d2823d05
Luis Parra <parra.chaconluis006@gmail.com>
2025-11-17 12:58:31 +0100

    cambios

--- Reto1_Luis_Laura/src/reto/funciones1.java
+++ Reto1_Luis_Laura/src/reto/funciones1.java
@@ -1,7 +1,20 @@
 package reto;
 
 public class funciones1 {
-	public static void main(String[] args) {
-		System.out.println("hola");
-	}
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    public boolean esPar(int n) {
+        return n % 2 == 0;
+    }
+
+    public boolean esImpar(int n) {
+        return n % 2 != 0;
+    }
 }
