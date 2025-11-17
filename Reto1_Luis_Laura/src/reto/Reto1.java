afd97f34cf00d310bbb9c1d7d1849bd5c2eb97bf
Luis Parra <parra.chaconluis006@gmail.com>
2025-11-17 12:54:47 +0100

    Funcion1 realizada y codigo del Reto1 agregado

--- Reto1_Luis_Laura/src/reto/Reto1.java
+++ Reto1_Luis_Laura/src/reto/Reto1.java
@@ -1,11 +1,43 @@
 package reto;
 
+import java.util.Scanner;
+
 public class Reto1 {
+	
+	//Función que pide por consola hasta que sea un número entero y lo devuelve
+	// Muestra el texto que le pasamos como parametro
+	public static int dimeEntero(String texto, Scanner sc)
+	{
+		do
+		{
+		try {
+			System.out.println(texto);
+			String s = sc.nextLine();
+			int n= Integer.parseInt(s);
+			return n;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hola");
-System.out.println();
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto. No es un número");
+		}
+		}while(true);
 	}
+	
+	//Función que pide por consola hasta que sea un número decimal y lo devuelve
+	// Muestra el texto que le pasamos como parametro
+	public static double dimeDouble(String texto, Scanner sc)
+	{
+		do
+		{
+		try {
+			System.out.println(texto);
+			String s = sc.nextLine();
+			double d= Double.parseDouble(s);
+			return d;
 
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto. No es un número");
+		}
+		}while(true);
+	}
 }
+
--- Reto1_Luis_Laura/src/reto/funciones1.java
+++ Reto1_Luis_Laura/src/reto/funciones1.java
@@ -1,5 +1,7 @@
 package reto;
 
 public class funciones1 {
-
+	public static void main(String[] args) {
+		System.out.println("hola");
+	}
 }
